5b84d15b227831e8575ecfa436575226eda61276
Joydeep Singha <joydeepsingha68@gmail.com>
2023-06-10 02:57:01 +0530

    feat: update polygon address airdrop address

--- src/constants/address.ts
+++ src/constants/address.ts
@@ -1,6 +1,6 @@
 export const AIRDROP_ADDRESS: { [chainId in number]: `0x${string}` } = {
     53_4353: '0xbCa3b1c5cd8f7b8C38503e94A170d6fE96a916c7',
-    137: '0xAAa0E0AF1ba7C9184cc85E4dC9f17686cd8F5332',
+    137: '0x2a83c6b8602a71093bEbAfcb1D1d1cb906e3F237',
 }
 export const TOKEN_FACTORY_ADDRESS: { [chainId in number]: `0x${string}` } = {
     53_4353: '0x9f73e26869e15571B925eaA2fFaE6De88B7100eA',
